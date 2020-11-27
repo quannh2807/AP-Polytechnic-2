dc9db08242d197a35219aae9a7dc272ebdf3337a
quannh2807 <helgrindxxx@gmail.com>
2020-11-27 20:17:33 +0700

    Fea: Create Bottom tab

--- src/navigations/app-navigator.js
+++ src/navigations/app-navigator.js
@@ -1,83 +1,85 @@
 import React from 'react';
-import { StatusBar, Text } from 'react-native';
+import { useWindowDimensions, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { Colors } from '_styles';
+import Home from '_navigations/home-navigator';
 
 const Drawer = createDrawerNavigator();
 
 const AppNavigator = () => {
+    const dimensions = useWindowDimensions();
+
+    const isLargeScreen = dimensions.width >= 768;
+
     return (
-        <>
-            <StatusBar backgroundColor={Colors.WHITE} barStyle="dark-content" />
+        <Drawer.Navigator
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+                drawerIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-            <Drawer.Navigator
-                initialRouteName="Home"
-                screenOptions={({ route }) => ({
-                    drawerIcon: ({ focused, color, size }) => {
-                        let iconName;
+                    switch (route.name) {
+                        case 'Home':
+                            iconName = focused
+                                ? 'home-outline'
+                                : 'home-outline';
+                            break;
+                        case 'PersonalInfomation':
+                            iconName = focused
+                                ? 'information'
+                                : 'information-outline';
+                            break;
+                        case 'Logout':
+                            iconName = focused ? 'log-out' : 'log-out-outline';
+                            break;
 
-                        switch (route.name) {
-                            case 'Home':
-                                iconName = focused
-                                    ? 'home-outline'
-                                    : 'home-outline';
-                                break;
-                            case 'TestSchedule':
-                                iconName = focused
-                                    ? 'clipboard-outline'
-                                    : 'clipboard-outline';
-                                break;
-                            case 'Transcript':
-                                iconName = focused
-                                    ? 'bar-chart-outline'
-                                    : 'bar-chart-outline';
-                                break;
-                            case 'SchoolHistory':
-                                iconName = focused
-                                    ? 'time-outline'
-                                    : 'time-outline';
-                                break;
-                            case 'Reward':
-                                iconName = focused
-                                    ? 'gift-outline'
-                                    : 'gift-outline';
-                                break;
+                        default:
+                            break;
+                    }
 
-                            default:
-                                break;
-                        }
-
-                        return (
-                            <Ionicons
-                                name={iconName}
-                                size={size}
-                                color={color}
-                            />
-                        );
-                    },
-                })}
-                drawerContentOptions={{
-                    labelStyle: {
-                        fontSize: 16,
-                    },
-                    activeTintColor: '#41a6dc',
-                    inactiveTintColor: 'gray',
+                    return (
+                        <Ionicons name={iconName} size={size} color={color} />
+                    );
+                },
+            })}
+            drawerType={isLargeScreen ? 'permanent' : 'back'}
+            drawerContentOptions={{
+                labelStyle: {
+                    fontSize: 16,
+                },
+                activeTintColor: Colors.PRIMARY,
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Drawer.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    title: 'Trang chủ',
                 }}
-            >
-                <Drawer.Screen
-                    name="Home"
-                    component={Home}
-                    options={{
-                        title: 'Home',
-                    }}
-                />
-            </Drawer.Navigator>
-        </>
+            />
+            <Drawer.Screen
+                name="PersonalInfomation"
+                component={PersonalInfomation}
+                options={{
+                    title: 'Thông tin cá nhân',
+                }}
+            />
+
+            <Drawer.Screen
+                name="Logout"
+                component={Logout}
+                options={{
+                    title: 'Đăng xuất',
+                }}
+            />
+        </Drawer.Navigator>
     );
 };
 
 export default AppNavigator;
 
-const Home = () => <Text>Home</Text>;
+const PersonalInfomation = () => <Text>Personal Infomation</Text>;
+const Logout = () => <Text>Log out</Text>;
---
+++ src/navigations/home-navigator.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { Colors } from '_styles';
+
+const HomeBottomTab = createBottomTabNavigator();
+
+const HomeNavigator = () => (
+    <HomeBottomTab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                switch (route.name) {
+                    case 'News':
+                        iconName = focused ? 'newspaper' : 'newspaper-outline';
+                        break;
+                    case 'Schedule':
+                        iconName = focused ? 'calendar' : 'calendar-outline';
+                        break;
+                    case 'Score':
+                        iconName = focused ? 'book' : 'book-outline';
+                        break;
+                    case 'Option':
+                        iconName = focused ? 'grid' : 'grid-outline';
+                        break;
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarLabel: ({ focused, color }) => {
+                let labelName;
+                switch (route.name) {
+                    case 'News':
+                        labelName = focused ? (
+                            <Text style={{ color }}>Tin tức</Text>
+                        ) : null;
+                        break;
+                    case 'Schedule':
+                        labelName = focused ? (
+                            <Text style={{ color }}>Lịch học</Text>
+                        ) : null;
+                        break;
+                    case 'Score':
+                        labelName = focused ? (
+                            <Text style={{ color }}>Điểm</Text>
+                        ) : null;
+                        break;
+                    case 'Option':
+                        labelName = focused ? (
+                            <Text style={{ color }}>Xem thêm</Text>
+                        ) : null;
+                        break;
+                }
+
+                return labelName;
+            },
+        })}
+        tabBarOptions={{
+            activeTintColor: Colors.PRIMARY,
+            inactiveTintColor: Colors.GRAY_DARK,
+            showLabel: false,
+            style: { paddingVertical: 8 },
+        }}
+        initialRouteName="News"
+    >
+        <HomeBottomTab.Screen name="News" component={NewsScreen} />
+        <HomeBottomTab.Screen name="Schedule" component={ScheduleScreen} />
+        <HomeBottomTab.Screen name="Score" component={ScoreScreen} />
+        <HomeBottomTab.Screen name="Option" component={OptionStack} />
+    </HomeBottomTab.Navigator>
+);
+
+export default HomeNavigator;
+
+const NewsScreen = () => <Text>News Screen</Text>;
+const ScheduleScreen = () => <Text>News Screen</Text>;
+const ScoreScreen = () => <Text>News Screen</Text>;
+const OptionStack = () => <Text>News Screen</Text>;
